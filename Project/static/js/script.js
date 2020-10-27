3bf7c2738d0d2aad7a6546b0a8c817cbcc0a95e9
SnehaAdki1996 <snehaadki9@gmail.com>
2020-10-27 15:28:47 +0530

    Project Og JS

--- Project/static/js/script.js
+++ Project/static/js/script.js
@@ -1,6 +1,8 @@
+
+let Example ;
+console.log(Example)
+
 //Challenge 1
-
-
 
 function ageInDays(){
     var birthYear = prompt("Enter Birth Year?")
@@ -23,4 +25,349 @@ function generateCat(){
     var div = document.getElementById('flex-cat-gen')
     image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small"
     div.appendChild(image)
+}
+
+//Challenge 3
+function rpsGame(yourChoice){
+    console.log(yourChoice)
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id
+    botChoice = numberToChoice(randToRpsInt())
+
+    result= decideWinner(humanChoice,botChoice); // [0 ,1 ] human Lost | bot won
+    console.log(result)
+    message = finalMessage(result) //You Won/lost/tied & color {"message": "you Won" ,"color" : "Green" }
+    console.log(message)
+    rpsFrontEnd(yourChoice.id,botChoice,message)//replace the div
+}
+
+function randToRpsInt(){
+    return Math.floor(Math.random(0,1,2)*3)
+}
+
+function numberToChoice(number){
+    return ['stone','paper','scissors'][number]
+}
+
+function decideWinner(yourChoice,computerChoice){
+    var rpsDataBase = {
+        'stone' :{
+            'scissors':1,
+            'stone':0.5,
+            'paper':0
+        },
+        'paper':{
+            'stone': 1,
+            'paper' : 0.5,
+            'scissors' : 0,
+        },
+        'scissors':{
+            'paper': 1,
+            'scissors' : 0.5,
+            'stone' : 0
+        }
+    }
+
+    var yourScore = rpsDataBase[yourChoice][computerChoice]
+    var computerScore = rpsDataBase[computerChoice][yourChoice]
+    return [yourScore , computerScore]
+}
+
+function finalMessage(result){
+    if(result[0]>result[1]){
+        return {
+            "message" : "You Won",
+            "color" : "green"
+        }
+    }
+    else if(result[1]>result[0]) {
+        return{
+            "message" :'You Lost',
+            "color":"red"
+        }
+    }
+    else{
+        return{
+            "message" :'You Tied',
+            "color":"yello"
+        }
+    }
+
+}
+
+function rpsFrontEnd(humanImageChoice,botImageChoice,finalMessage){
+    var imagesDatabase = {
+        'stone' : document.getElementById('stone').src,
+        'paper' : document.getElementById('paper').src,
+        'scissors' : document.getElementById('scissors').src
+    }
+
+    document.getElementById('stone').remove()
+    document.getElementById('paper').remove()
+    document.getElementById('scissors').remove()
+
+    var humanDiv = document.createElement('div')
+    var botDiv = document.createElement('div')
+    var messageDiv = document.createElement('div')
+    messageDiv.innerHTML = "<h1 style='color:" +finalMessage['color'] +"; font-size: 60px; padding : 30px;'>"+finalMessage['message']+"</h1>"
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice]+"' height=150 width=150 style='box-shadow:0px 10px 50px rgba(243,38,24,1)'>"
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice]+"' height=150 width=150 style='box-shadow:0px 10px 50px rgba(37,50,233,1)'>"
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv)
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv)
+    document.getElementById('flex-box-rps-div').appendChild(botDiv)
+}
+
+
+//Challenge 4
+var all_buttons = document.getElementsByTagName('button')
+
+var copyAllButtons = []
+
+for(let i=0;i<all_buttons.length ;i++){
+    copyAllButtons.push(all_buttons[i].classList[1])
+}
+
+
+
+function buttonColorChange(buttonThingy){
+    if(buttonThingy.value === 'red'){
+        buttonRed()
+    }
+    else if (buttonThingy.value === 'green'){
+        buttonGreen()
+    }
+    else if (buttonThingy.value === 'reset'){
+        buttonReset()
+    }
+    else if(buttonThingy.value === 'random'){
+        buttonRandom();
+    }
+
+}
+
+function buttonRed(){
+    for(var i=0;i<all_buttons.length ;i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        all_buttons[i].classList.add('btn-danger')
+    }   
+}
+
+function buttonGreen(){
+    for(var i=0;i<all_buttons.length ;i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        all_buttons[i].classList.add('btn-success')
+    }   
+}
+
+function buttonRandom(){
+    for(var i=0;i<all_buttons.length ;i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        var randomClassData = randomClass()
+        all_buttons[i].classList.add(randomClassData)
+    }   
+}
+
+function randomClass(){
+    return copyAllButtons[Math.floor(Math.random(all_buttons.length)*7)]
+}
+
+function buttonReset(){
+    for(var i=0;i<all_buttons.length ;i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        
+        all_buttons[i].classList.add(copyAllButtons[i])
+    }   
+}
+
+//Challenge 5
+
+let blackJackGame = {
+    'you':{
+        "scorespan":"#your-blackjack-result",
+        "div":"#your-box",
+        "score":0
+    },
+    'dealer':{
+        "scorespan":"#dealer-blackjack-result",
+        "div":"#dealer-box",
+        "score":0
+    },
+    "cards":['2','3','4','5','6','7','8','9','A','K','Q','J'],
+    "cardsMap":{'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'A':[1,11],'K':10,'Q':10,'J':10},
+    'wins' : 0,
+    'losses' : 0,
+    'draws' : 0,
+    'isStand' : false, //stand mode activated
+    'turnsOver' : false, // all turns over we can click deal
+}
+
+const YOU = blackJackGame['you']
+const DEALER = blackJackGame['dealer']
+
+const hitSound = new Audio('static/sounds/swish.m4a')
+const winSound = new Audio('static/sounds/cash.mp3')
+const lostSound = new Audio('static/sounds/aww.mp3')
+
+document.querySelector('#blackjack-hit-button').addEventListener('click',blackJackHit);
+document.querySelector('#blackjack-stand-button').addEventListener('click',blackJackStand);
+document.querySelector('#blackjack-deal-button').addEventListener('click',blackJackDeal);
+
+var result = 0;
+
+function blackJackHit(){
+    if(blackJackGame['isStand'] === false){
+        let card = randomCard()
+        showCard(YOU,card);
+        updateScore(YOU,card)
+        showScore(YOU)
+    }
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve,ms))
+}
+
+
+async function blackJackStand(){
+    blackJackGame['isStand'] = true;
+    while(DEALER['score'] < 15 && (blackJackGame['isStand'] === true)){
+        let card = randomCard()
+        showCard(DEALER,card);
+        updateScore(DEALER , card)
+        showScore(DEALER)
+        await sleep(10000)
+    }
+    //if(DEALER['score']>15){
+        showResult(computeWinner())
+        blackJackGame['turnsOver'] = true;
+    //}
+}
+
+function showCard(activePlayer,card){
+    if(activePlayer['score'] <= 21){
+        let cardImage = document.createElement('img')
+        //`static/images/${card}.png`
+        cardImage.src = 'static/images/'+card+'.png';
+        document.querySelector(activePlayer['div']).appendChild(cardImage)
+        hitSound.play()
+    }
+}
+
+function updateScore(activePlayer,card){
+    //If adding 11 keeps me below 21 add 11 Otherwise add 1
+    console.log(blackJackGame['cardsMap'][card].length)
+    console.log([card])
+    if(blackJackGame['cardsMap'][card].length > 1){
+        
+        if ((blackJackGame['cardsMap'][card] + activePlayer['score']) <= 21){
+            activePlayer['score'] += blackJackGame['cardsMap'][card][1];    
+        }
+        else{
+            activePlayer['score'] += blackJackGame['cardsMap'][card][0];    
+        }
+    }else{
+        activePlayer['score'] += blackJackGame['cardsMap'][card];
+    }
+}
+
+function showScore(activePlayer){
+    if(activePlayer['score'] > 21){
+        document.querySelector(activePlayer['scorespan']).textContent = "Bust!.."
+        document.querySelector(activePlayer['scorespan']).style.color = 'red'
+    }
+    else{
+        document.querySelector(activePlayer['scorespan']).textContent = activePlayer['score']
+    }
+}
+
+//Computer winner & Looser
+function computeWinner(){
+    let winner;
+    if(YOU['score'] <= 21){
+        if(YOU['score'] > DEALER['score'] || (DEALER['score'] > 21)){
+            console.log('You Win!..')
+            blackJackGame['wins']++;
+            winner = YOU
+        }else if(YOU['score'] < DEALER['score']){
+            console.log("you lost!...")
+            blackJackGame['losses']++;
+            winner = DEALER
+        }
+        else if(YOU['score'] === DEALER['score']){
+            console.log('Tied!...')
+            blackJackGame['draws']++;
+        }
+    }else if(YOU['score'] > 21 && DEALER['score']<= 21){
+        console.log('You Loast!...')
+        blackJackGame['losses']++;
+        winner = DEALER
+    }
+    else if(YOU['score'] > 21 && DEALER['score'] > 21){
+        console.log('YOU Drew!...')
+        blackJackGame['draws']++;
+
+    }
+    console.log('Winner '+ winner )
+    return winner
+}
+
+function randomCard(){
+    let card_no = blackJackGame['cards'][(Math.floor( Math.random()*12))]
+    console.log(card_no)
+    return card_no;
+}
+
+function blackJackDeal(){
+    if (blackJackGame['turnsOver']){
+    //showResult(computeWinner())
+        blackJackGame['isStand'] = false
+        let yourImages = document.querySelector('#your-box').querySelectorAll('img')
+        let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img')
+        console.log(yourImages)
+        for(let i=0;i<yourImages.length ; i++){
+            yourImages[i].remove()
+        }
+        for(let i=0;i<dealerImages.length ; i++){
+            dealerImages[i].remove()
+        }
+
+        YOU['score'] = 0
+        DEALER['score'] = 0
+
+        document.querySelector('#your-blackjack-result').textContent = 0;
+        document.querySelector('#your-blackjack-result').style = "#ffffff"
+        document.querySelector('#dealer-blackjack-result').textContent = 0;
+        document.querySelector('#dealer-blackjack-result').style = "white"
+        document.querySelector('#blackjack-result').textContent = "Let's play"
+        document.querySelector('#blackjack-result').style.color = 'black'
+        blackJackGame['turnsOver'] = false
+    }
+}
+
+function showResult(winner){
+    if(blackJackGame['turnsOver'] === true){
+        let message, messageColor;
+    if( winner === YOU){
+        message = 'You Won!....'
+        messageColor = 'green'
+        winSound.play()
+    }
+    else if(winner === DEALER){
+        message = 'You Lost!...'
+        messageColor = 'red'
+        lostSound.play();
+    }
+    else {
+        message = "You Drew!.."
+        messageColor = 'black'
+    }
+
+    document.querySelector('#blackjack-result').textContent = message
+    document.querySelector('#blackjack-result').style.color = messageColor
+    document.querySelector('#wins').textContent = blackJackGame['wins']
+    document.querySelector('#loses').textContent = blackJackGame['losses']
+    document.querySelector('draw').textContent = blackJackGame['draws']
+
+    }
+    
 }
--- home.js
+++ home.js
@@ -1,220 +1,305 @@
-console.log("Hello")
+// console.log("Hello")
 
-//alert("Hey")
+// //alert("Hey")
 
-var b = "Some Thing"
-console.log(b)
-// var age = prompt('What is your Age?');
+// var b = "Some Thing"
+// console.log(b)
+// // var age = prompt('What is your Age?');
 
-// console.log(age)
+// // console.log(age)
 
-//document.getElementById('someText').innerHTML = age
+// //document.getElementById('someText').innerHTML = age
 
-//Numer operations
-var num1 = 10
-//All Arithmetic operation
-console.log(num1 + 10);
-//Increment both ways
-console.log("Post Increment " + num1++)
-console.log("Increment 2nd way", +num1 + 1)
+// //Numer operations
+// var num1 = 10
+// //All Arithmetic operation
+// console.log(num1 + 10);
+// //Increment both ways
+// console.log("Post Increment " + num1++)
+// console.log("Increment 2nd way", +num1 + 1)
 
-//Decrement
-console.log("Post Increment " + num1--)
-console.log("Increment 2nd way", +num1 - 1)
+// //Decrement
+// console.log("Post Increment " + num1--)
+// console.log("Increment 2nd way", +num1 - 1)
 
-//Reminder
-console.log("Reminder", 40 % 6)
+// //Reminder
+// console.log("Reminder", 40 % 6)
 
-//Quotion
-console.log("Quotient", 9 / 3)
+// //Quotion
+// console.log("Quotient", 9 / 3)
 
-num = 20
-num += 20
-console.log("Short Hand Operator", num)
+// num = 20
+// num += 20
+// console.log("Short Hand Operator", num)
 
 
-//Functions:
+// //Functions:
 
-//Define a function
-//Call a function
-function greeting(name) {
+// //Define a function
+// //Call a function
+// function greeting(name) {
 
-    document.getElementById("someText").innerHTML = "Hello " + name
+//     document.getElementById("someText").innerHTML = "Hello " + name
+// }
+
+// //var name = prompt("Name Please?")
+
+// //greeting(name)
+
+
+// // String + num = String
+// // num + num = num
+
+// function sunNumber(no1, no2) {
+//     console.log(no1 + no2)
+// }
+
+// //sunNumber(8,50)
+
+
+
+// //Loops
+
+// /// 1 While Loop
+// /// 2 For Loop
+
+
+
+// var num = 0
+
+// while (num < 5) {
+//     console.log("num")
+//     num += 1
+// }
+
+// // For Loop
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+
+
+// //Data Type in JS
+// let yourName = "Sneha" //string
+// let youAge = 50        //Int
+// let name = { fname: 'Sneha', last: 'cad' }       //Object
+// let truth = true //Boolean
+// let grocessary = ['apple', 'banana', 'Oranges'] // Array
+// let random = undefined // undefined
+// let nothing = null // Value null
+
+
+// //String Common Methods
+
+// var fruit = "Banana,Apple,Orange"
+// let moreFruit = "Banana\nApple" //New Line '\n' new Line
+// console.log(moreFruit)
+// console.log(fruit.length)
+// console.log(fruit.indexOf('qn')) //-1
+// console.log(fruit.indexOf('an')) //1
+// console.log(fruit.indexOf('nan')) //2
+// console.log(fruit.slice(2, 4)) //na
+// console.log(fruit.replace('Ban', '123')) //123ana
+// console.log(fruit.toLocaleLowerCase()) //banana
+// console.log(fruit.toLocaleUpperCase()) //BANANA
+// console.log(fruit.charAt(1)) //a
+// console.log(fruit.split(',')) //["Banana", "Apple", "Orange"]
+// console.log(fruit.split(''))// ["B", "a", "n", "a", "n", "a", ",", "A", "p", "p", "l", "e", ",", "O", "r", "a", "n", "g", "e"] //split by character
+
+
+// //Arrays in JS
+// let fruits = ['Banana', 'Orange', 'Apple', 'Cheery'] //one way
+// fruits = new Array('Banana', 'Orange', 'Apple', 'Cheery') //2nd Way
+
+// console.log(fruits[0]) // Banana // Access Value at index
+// fruits[0] = "Pear" //REplace or change
+// console.log(fruits)
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i])
+// }
+
+// //Common MEthods
+// console.log(fruits.toString())//Convert to string
+// console.log(fruits.join('-')) //taken an array join by '-'
+// console.log(fruits.pop(), fruits) // removed last element
+// console.log(fruits.push('Pineapple'), fruits) // Adds at last
+// console.log(fruits[fruits.length])  //undefiend becz no value
+// fruits[fruits.length] = "BlackBerry" // Adds
+// console.log(fruits)
+// fruits.shift() // Removes frist element
+// console.log(fruits)
+// fruits.unshift('Kivi') //Add first elemet  in an array
+// console.log(fruits)
+
+// let veg = ['Spinach', 'Brocoli']
+// let allGRoceries = fruits.concat(veg) // joins 2 array
+// console.log(allGRoceries)
+
+// //slice an array
+
+// console.log(allGRoceries.slice(1, 4)) // from index 1 to 4
+
+// //REverse an array
+// console.log(allGRoceries.reverse())
+
+// console.log(allGRoceries.sort())
+
+// let someNumber = [1, 5, 3, 457647, 67, 867, 8]
+// //Sort
+// console.log(someNumber.sort(function (a, b) { return a - b })) //Ascending ORder b-a in descending
+
+
+// let emptyArray = new Array()
+
+// for (let i = 0; i < 10; i++) {
+//     emptyArray.push(i)
+// }
+// console.log(emptyArray)
+
+// //Objects
+
+// studet = {
+//     fname: 'Sneha',
+//     lname: 'Konkati',
+//     age: 20,
+
+//     studentInfo: function () {
+//         return this.fname + " " + this.lname
+//     }
+// }
+
+// console.log(studet)
+// console.log(studet.fname)
+// studet.height = 1.7
+// console.log(studet.height)
+
+// studet.height = 2.0 //reassign the value
+// console.log(studet)
+
+// console.log(studet.studentInfo())
+
+
+// //If else Conditional & switch
+// //Boolean Operator 
+// //&& And
+// //|| OR
+// //var age = prompt("Your Age")
+// var age = 8
+
+// if (age >= 18 && age <= 35) {
+//     status = "target Demo"
+// }
+// else {
+//     status = "not my Target Demo"
+// }
+
+// console.log(status)
+
+
+// //SWITCH 
+// // Differentiate Week Day vs Week End
+
+// // var day = parseInt(prompt("Enter Day in number"));
+// var day = 10
+// switch (day) {
+//     case 0:
+//         console.log("Weekend")
+//         break;
+//     case 5:
+//         console.log("Weekend")
+//         break;
+//     case 6:
+//         console.log("Weekend")
+//         break;
+//     default:
+//         console.log("Weekday")
+// }
+
+// let let1 = 20;
+// //let let1 =30
+// console.log(let1)
+
+// //Above scenario throws error i.e let1 already declared
+
+
+// var var1 = 20;
+// var var1 = 30;
+// console.log(var1)
+
+
+
+var a =['a','b','c']
+var b = a 
+var c = a.slice()
+a.push('date')
+console.log(a,b)
+
+console.log('1' == 1)
+console.log('1' === 1)
+
+console.log(false == 0)
+console.log(false === 0)
+
+console.log(false == '')
+console.log(false === '')
+
+console.log('abc' == 'abc')
+console.log([1,2,3] == [1,2,3])
+
+
+function foo(a,b,c){
+    a++;
+    b = "new String"
+    c['key'] = "new Value"
 }
 
-//var name = prompt("Name Please?")
+var a = 1,
+    b= "old String",
+    c ={"key" : "old Value"}
 
-//greeting(name)
-
-
-// String + num = String
-// num + num = num
-
-function sunNumber(no1, no2) {
-    console.log(no1 + no2)
-}
-
-//sunNumber(8,50)
+    foo(a,b,c)
+    console.log(a,b,c)
 
 
+    var counter = ( function(){
+        var mycounter =0;
+        function changeBy(val){
+            mycounter+= val
+        }
 
-//Loops
-
-/// 1 While Loop
-/// 2 For Loop
-
-
-
-var num = 0
-
-while (num < 5) {
-    console.log("num")
-    num += 1
-}
-
-// For Loop
-
-for (let i = 0; i < 5; i++) {
-    console.log(i)
-}
-
-
-
-//Data Type in JS
-let yourName = "Sneha" //string
-let youAge = 50        //Int
-let name = { fname: 'Sneha', last: 'cad' }       //Object
-let truth = true //Boolean
-let grocessary = ['apple', 'banana', 'Oranges'] // Arrya
-let random = undefined // undefined
-let nothing = null // Value null
-
-
-//String Common Methods
-
-var fruit = "Banana,Apple,Orange"
-let moreFruit = "Banana\nApple" //New Line '\n' new Line
-console.log(moreFruit)
-console.log(fruit.length)
-console.log(fruit.indexOf('qn')) //-1
-console.log(fruit.indexOf('an')) //1
-console.log(fruit.indexOf('nan')) //2
-console.log(fruit.slice(2, 4)) //na
-console.log(fruit.replace('Ban', '123')) //123ana
-console.log(fruit.toLocaleLowerCase()) //banana
-console.log(fruit.toLocaleUpperCase()) //BANANA
-console.log(fruit.charAt(1)) //a
-console.log(fruit.split(',')) //["Banana", "Apple", "Orange"]
-console.log(fruit.split(''))// ["B", "a", "n", "a", "n", "a", ",", "A", "p", "p", "l", "e", ",", "O", "r", "a", "n", "g", "e"] //split by character
-
-
-//Arrays in JS
-let fruits = ['Banana', 'Orange', 'Apple', 'Cheery'] //one way
-fruits = new Array('Banana', 'Orange', 'Apple', 'Cheery') //2nd Way
-
-console.log(fruits[0]) // Banana // Access Value at index
-fruits[0] = "Pear" //REplace or change
-console.log(fruits)
-
-for (let i = 0; i < fruits.length; i++) {
-    console.log(fruits[i])
-}
-
-//Common MEthods
-console.log(fruits.toString())//Convert to string
-console.log(fruits.join('-')) //taken an array join by '-'
-console.log(fruits.pop(), fruits) // removed last element
-console.log(fruits.push('Pineapple'), fruits) // Adds at last
-console.log(fruits[fruits.length])  //undefiend becz no value
-fruits[fruits.length] = "BlackBerry" // Adds
-console.log(fruits)
-fruits.shift() // Removes frist element
-console.log(fruits)
-fruits.unshift('Kivi') //Add first elemet  in an array
-console.log(fruits)
-
-let veg = ['Spinach', 'Brocoli']
-let allGRoceries = fruits.concat(veg) // joins 2 array
-console.log(allGRoceries)
-
-//slice an array
-
-console.log(allGRoceries.slice(1, 4)) // from index 1 to 4
-
-//REverse an array
-console.log(allGRoceries.reverse())
-
-console.log(allGRoceries.sort())
-
-let someNumber = [1, 5, 3, 457647, 67, 867, 8]
-//Sort
-console.log(someNumber.sort(function (a, b) { return a - b })) //Ascending ORder b-a in descending
-
-
-let emptyArray = new Array()
-
-for (let i = 0; i < 10; i++) {
-    emptyArray.push(i)
-}
-console.log(emptyArray)
-
-//Objects
-
-studet = {
-    fname: 'Sneha',
-    lname: 'Konkati',
-    age: 20,
-
-    studentInfo: function () {
-        return this.fname + " " + this.lname
+        return{
+            inc : function(){
+                changeBy(1)
+            },
+            dec : function(){
+                changeBy(-1)
+            },
+            value : function(){
+                return mycounter
+            }
+        }
     }
-}
 
-console.log(studet)
-console.log(studet.fname)
-studet.height = 1.7
-console.log(studet.height)
+    )();
 
-studet.height = 2.0 //reassign the value
-console.log(studet)
+    console.log(counter.value())
+    counter.inc();
+    counter.inc()
+    console.log(counter.value())
+    counter.dec();
+    console.log(counter.value())
+    // counter.changeBy(2)
+    console.log(counter.value())
 
-console.log(studet.studentInfo())
+    var x =2
+    var y  =4
+    if((y>x || y++ === 4)&& ++y === 5){
+        x =1
+    }
+    else{
+        x = 4
+    }
 
-
-//If else Conditional & switch
-//Boolean Operator 
-//&& And
-//|| OR
-//var age = prompt("Your Age")
-var age = 8
-
-if (age >= 18 && age <= 35) {
-    status = "target Demo"
-}
-else {
-    status = "not my Target Demo"
-}
-
-console.log(status)
-
-
-//SWITCH 
-// Differentiate Week Day vs Week End
-
-var day = parseInt(prompt("Enter Day in number"));
-switch (day) {
-    case 0:
-        console.log("Weekend")
-        break;
-    case 5:
-        console.log("Weekend")
-        break;
-    case 6:
-        console.log("Weekend")
-        break;
-    default:
-        console.log("Weekday")
-}
-
-
-
+    console.log(x,y)
